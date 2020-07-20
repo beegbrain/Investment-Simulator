9798b6758763708e7f431ffb590584930133e59e
Jell01 <67522272+Jell01@users.noreply.github.com>
2020-07-20 13:53:40 -0700

    Add files via upload

---
+++ DataImport.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+api_key = "2PNMXB7PYSF8WJYZ"
+
+ts = TimeSeries(key = api_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='AAPL', interval='1min',outputsize='full')
+print(data)
