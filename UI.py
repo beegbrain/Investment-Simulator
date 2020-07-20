767b447dc03e4ffb58b3989e1141e3f35c57eda8
beegbrain <67524127+beegbrain@users.noreply.github.com>
2020-07-20 13:11:30 -0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -7,10 +7,14 @@ app.title("Investment Simulator")
 app.geometry('1920x1080')
 app.configure(bg='black')
 
+menubutton = Menubutton(app, text= "≡")
+menubutton.menu = Menu(menubutton)   
+menubutton["menu"]= menubutton.menu 
+  
+text = tkinter.Text(app, height=1, width=5, bg = 'black', fg = 'white', relief=FLAT)
+text.configure(font=("Calibri", 12, ""))
+text.insert(tkinter.END, "Hello")
 
-T = tkinter.Text(app, height=1, width=5, bg = 'black', fg = 'white', relief=FLAT)
-T.place(x=10,y=30)
-T.configure(font=("Calibri", 12, ""))
-T.insert(tkinter.END, "Hello")
-    
+text.place(x=10,y=60)
+menubutton.place(x=10,y=30)
 app.mainloop()
