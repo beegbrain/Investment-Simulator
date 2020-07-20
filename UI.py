826c5254641e6dafb4ce2c26f97e43d1cec0da68
beegbrain <67524127+beegbrain@users.noreply.github.com>
2020-07-20 11:29:52 -0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,10 +1,15 @@
 import tkinter
+from tkinter import *
+
 app = tkinter.Tk()
 app.title("Investment Simulator")
 app.geometry('1920x1080')
-app.configure(bg='black')
-T = tkinter.Text(app, height=1, width=5)
+
+
+
+T = tkinter.Text(app, height=1, width=5, bg = 'black', fg = 'white')
 T.place(x=10,y=30)
 T.configure(font=("Lucida Console", 12, ""))
 T.insert(tkinter.END, "Hello")
+    
 app.mainloop()
