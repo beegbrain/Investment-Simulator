f24ba933ac36609363712b5d63b3d2d66f1da4e7
beegbrain <67524127+beegbrain@users.noreply.github.com>
2020-07-20 13:59:23 -0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,20 +1,40 @@
 import tkinter
 from tkinter import *
 import numpy
+global page
+def switch_home():
+    cur_bal_txt .destroy()
+    text = tkinter.Text(app, height=1, bg = 'black', fg = 'grey', relief=FLAT)
+    text.configure(font=("Calibri", 30, ""))
+    text.insert(tkinter.END, "Dunno")
+    text.grid(row=1,column=1)
+    print("pog")
+def switch_portfolio():
+    global page
+    page = "Portfolio"
+def switch_market():
+    global page
+    page = "Market"
 
 app = tkinter.Tk()
 app.title("Investment Simulator")
-app.geometry('1920x1080')
+app.geometry('1000x1080')
 app.configure(bg='black')
 
 menubutton = Menubutton(app, text= "≡")
-menubutton.menu = Menu(menubutton)   
+menubutton.grid()
+menubutton.menu = Menu(menubutton)
 menubutton["menu"]= menubutton.menu 
-  
-text = tkinter.Text(app, height=1, width=5, bg = 'black', fg = 'white', relief=FLAT)
-text.configure(font=("Calibri", 12, ""))
-text.insert(tkinter.END, "Hello")
+menubutton.menu.add_command(label="Home", command=switch_home)
+menubutton.menu.add_command(label="Portfolio", command=switch_portfolio)
+menubutton.menu.add_command(label="Market", command=switch_market)
 
-text.place(x=10,y=60)
-menubutton.place(x=10,y=30)
+cur_bal_txt = tkinter.Text(app, height=1, bg = 'black', fg = 'grey', relief=FLAT)
+cur_bal_txt.configure(font=("Calibri", 30, ""))
+cur_bal_txt.insert(tkinter.END, "Your Balance:")
+
+menubutton.grid(row=0,column=0)
+cur_bal_txt.grid(row=1,column=1)
 app.mainloop()
+
+
