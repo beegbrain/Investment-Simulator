0220c285afe0e770f2795d91f5de9520cdb7f7c3
beegbrain <67524127+beegbrain@users.noreply.github.com>
2020-07-20 15:57:33 -0700

    Update UI.py
    the visual is still fucked up

--- UI.py
+++ UI.py
@@ -6,49 +6,44 @@ root = tkinter.Tk()
 root.title("Investment Simulator")
 root.geometry('1000x1080')
 root.configure(bg='black')
-frame = Frame(root)
+home = Frame(root)
+market = Frame(root)
+portfolio = Frame(root)
 
-def startup():
-    frame.configure(bg='black')
-    frame.pack()
-    menubutton = Menubutton(frame, text= "≡")
-    menubutton.menu = Menu(menubutton)
-    menubutton["menu"]= menubutton.menu 
-    menubutton.menu.add_command(label="Home", command=page_home)
-    menubutton.menu.add_command(label="Portfolio", command=page_portfolio)
-    menubutton.menu.add_command(label="Market", command=page_market)
-    menubutton.grid(row=0,column=0,pady=20,padx=20)
+def raise_frame(frame):
+    frame.tkraise()
 
-def page_home():
-    cur_bal_txt = tkinter.Text(frame, height=1, bg = 'black', fg = 'grey', relief=FLAT)
-    cur_bal_txt.configure(font=("Calibri", 30, ""))
-    cur_bal_txt.insert(tkinter.END, "Your Balance:")
-    cur_bal_txt.grid(row=1,column=1)
-    frame.grid_forget()
-    return([cur_bal_txt])
-def page_portfolio():
-    equity = "100,000,000"
-    for widget in page_home()[1:]:
-        print(widget)
-        widget.destroy()
-    frame.grid_forget()
-    equity_txt = tkinter.Text(frame, height=1, bg = 'black', fg = 'grey', relief=FLAT)
-    equity_txt.configure(font=("Calibri", 30, ""))
-    equity_txt.insert(tkinter.END, "Your Equity:")
-    
-    equity = tkinter.Text(frame, height=1, bg = 'black', fg = 'white', relief=FLAT)
-    equity.configure(font=("Calibri", 50, ""))
-    equity_txt.insert(tkinter.END, "100,000,000")
-    
-    equity_txt.grid(row=1,column=1)
-    equity.grid(row=2,column=1)
-    
-def page_market():
-    pass
+for frame in (home, market, portfolio):
+    frame.grid(row=0, column=0, sticky='news')
 
-startup()
-page_home()
+#Switch Pages
+Button(home, text='Home',fg='white', bg='black',command=lambda:raise_frame(home)).pack(side='left')
+Button(home, text='Market',fg='white', bg='black', command=lambda:raise_frame(market)).pack()
+Button(home, text='Portfolio',fg='white', bg='black', command=lambda:raise_frame(portfolio)).pack(side='right')
 
+Button(market, text='Home', fg='white', bg='black',command=lambda:raise_frame(home)).pack()
+Button(market, text='Market', fg='white', bg='black',command=lambda:raise_frame(market)).pack()
+Button(market, text='Portfolio', fg='white', bg='black',command=lambda:raise_frame(portfolio)).pack()
+
+Button(portfolio, text='Home', fg='white', bg='black',command=lambda:raise_frame(home)).pack()
+Button(portfolio, text='Market', fg='white', bg='black',command=lambda:raise_frame(market)).pack()
+Button(portfolio, text='Portfolio', fg='white', bg='black',command=lambda:raise_frame(portfolio)).pack()
+
+#Home
+cur_bal_txt = tkinter.Text(home, bg = 'white', fg = 'grey', relief=FLAT)
+cur_bal_txt.configure(font=("Calibri", 30, ""))
+cur_bal_txt.insert(tkinter.END, "Your Balance:")
+cur_bal_txt.pack()
+cur_bal_txt.config(state=DISABLED)
+ 
+#Portfolio 
+equity = "100,000,000"
+equity_txt = tkinter.Text(portfolio, height=1, bg = 'black', fg = 'grey', relief=FLAT)
+equity_txt.configure(font=("Calibri", 30, ""))
+equity_txt.insert(tkinter.END, "Your Equity:")
+equity_txt.insert(tkinter.END, "100,000,000")
+
+equity_txt.place()
+
+raise_frame(home)
 root.mainloop()
-
-
