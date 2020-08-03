755d85ec91916a47dee389cb3af17bbac03f37e9
maxy1991991 <67882752+maxy1991991@users.noreply.github.com>
2020-08-03 09:16:00 -0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -433,13 +433,113 @@ def raise_watchlist():
 def raise_market():
     global visible
     visible = 'market'
-    print('now on MARKET')
     market.tkraise()
     graph = tkinter.Text(market, bg = 'black', fg = 'grey', relief=FLAT,height=1)
-    graph.configure(font=("Calibri", 30, ""))
-    graph.insert(tkinter.END, "How do i graph")
+    def year(ticker):
+        ticker = yf.Ticker(ticker)
+        ticker.info
+        yearpricelist=list()
+        for f, b in zip(ticker.history(period="1y",interval="5d")["Open"], ticker.history(period="1y",interval="5d")["Close"]):
+            yearpricelist.append( 100 * (b - f) / f)
+        yearprice= np.array(yearpricelist)
+        yeartime=list(range(0,len(yearpricelist)))
+        return (yeartime,yearprice)
+    
+    def day(ticker):
+        ticker = yf.Ticker(ticker)
+        ticker.info
+        daypricelist=list()
+        for f, b in zip(ticker.history(period="1d",interval="5m")["Open"], ticker.history(period="1d",interval="5m")["Close"]):
+            daypricelist.append( 100 * (b - f) / f)
+        dayprice= np.array(daypricelist)
+        daytime=list(range(0,len(daypricelist)))
+        return (daytime,dayprice)
+
+    def week(ticker):
+        weekpricelist=list()
+        ticker = yf.Ticker(ticker)
+        ticker.info
+        for f, b in zip(ticker.history(period="5d",interval= "1h")["Open"], ticker.history(period="5d",interval= "1h")["Close"]):
+            weekpricelist.append( 100 * (b - f) / f)    x_coor  
+        weekprice= np.array(weekpricelist)
+        weektime=list(range(0,len(weekpricelist)))
+        return (weektime,weekprice)
+
+    fig = Figure(figsize=(6,6))#increase to make plot bigger
+    a = fig.add_subplot(111)#scale??? bigger the number, the smaller the size
+    def weekgraph():
+        try:
+            canvas.delete('all')
+        except:
+            pass
+        a.plot(week('^IXIC')[0],week('^IXIC')[1],color='#E5CFAD')
+        a.plot(week('^DJI')[0],week('^DJI')[1],color='#D392A4')
+        a.plot(week('^GSPC')[0],week('^GSPC')[1],color='#98B7C3')
+        a.set_facecolor('black')    
+        a.set_title ("Market", fontsize=16)
+        a.set_ylabel("Price", fontsize=14)
+        a.set_xlabel("Hour", fontsize=14)  
+        canvas = FigureCanvasTkAgg(fig, master=market)
+        canvas.get_tk_widget().place(x=100,y=100)
+        canvas.draw()
+
+    def yeargraph():
+        try:
+            canvas.delete('all')
+        except:
+            pass
+        a.plot(year('^IXIC')[0],year('^IXIC')[1],color='#E5CFAD')
+        a.plot(year('^DJI')[0],year('^DJI')[1],color='#D392A4')
+        a.plot(year('^GSPC')[0],year('^GSPC')[1],color='#98B7C3')
+        a.set_facecolor('black')
+        a.set_title ("Market", fontsize=16)
+        a.set_ylabel("Price", fontsize=14)
+        a.set_xlabel("5 Days", fontsize=14)  
+        canvas = FigureCanvasTkAgg(fig, master=market)
+        canvas.get_tk_widget().place(x=100,y=100)
+        canvas.draw()
+
+    def daygraph():
+        try:
+            canvas.delete('all')
+        except:
+            pass
+        a.plot(day('^IXIC')[0],day('^IXIC')[1],color='#E5CFAD')
+        a.plot(day('^DJI')[0],day('^DJI')[1],color='#D392A4')
+        a.plot(day('^GSPC')[0],day('^GSPC')[1],color='#98B7C3')
+        a.set_facecolor('black')
+        a.set_title ("Market", fontsize=16)
+        a.set_ylabel("Price", fontsize=14)
+        a.set_xlabel("5 minutes", fontsize=14)  
+        canvas = FigureCanvasTkAgg(fig, master=market)
+        canvas.get_tk_widget().place(x=100,y=100)
+        canvas.draw()
+
+    Button(market, text='Past day',fg='black', bg='grey', relief=FLAT, command=lambda:daygraph()).place(x=0,y=300)
+    Button(market, text='Past 5 days',fg='black', bg='grey', relief=FLAT, command=lambda:weekgraph()).place(x=0,y=400) 
+    Button(market, text='Past year',fg='black', bg='grey', relief=FLAT, command=lambda:yeargraph()).place(x=0,y=500)    
+
     graph.place(x=100,y=400)
     graph.config(state=DISABLED)
+
+    legend = tkinter.Text(market, height=1, width=7, bg = '#E5CFAD', fg = 'black', relief=FLAT)
+    legend.configure(font=("Calibri", 15, ""))
+    legend.insert(tkinter.END, "Nasdaq")
+    legend.place(x=0,y=550)
+    legend.config(state=DISABLED)
+
+    legend1 = tkinter.Text(market, height=1, width=9, bg = '#D392A4', fg = 'black', relief=FLAT)
+    legend1.configure(font=("Calibri", 15, ""))
+    legend1.insert(tkinter.END, "Dow Jones")
+    legend1.place(x=0,y=600)
+    legend1.config(state=DISABLED)
+
+    legend2 = tkinter.Text(market, height=1, width=8, bg = '#98B7C3', fg = 'black', relief=FLAT)
+    legend2.configure(font=("Calibri", 15, ""))
+    legend2.insert(tkinter.END, "S & P 500")
+    legend2.place(x=0,y=650)
+    legend2.config(state=DISABLED)
+
 
 equity = 0
 def updatePortfolio():
